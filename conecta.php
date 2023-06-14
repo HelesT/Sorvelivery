d8f02ec5b547bb025c4c581605c524d0872d012d
HelesT <nyltondudu29@gmail.com>
2023-06-14 12:29:12 -0300

    a

--- conecta.php
+++ conecta.php
@@ -1,13 +1,14 @@
 <?php
-    // DEFININDO O FUSO HORÁRIO:
-    date_default_timezone_set('America/Sao_Paulo');
-    
-    // CONEXÃO COM A BASE DE DADOS: verifique sempre o dbname
-    try{
-        $pdo = new PDO("mysql:dbname=sorvelivery;host=localhost;charset=utf8","root","");
+include("conecta.php");
+
+$query = "SELECT nome_produto FROM produtos";
+$result = $pdo->query($query);
+
+if ($result) {
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        $nome_produto = $row['nome_produto'];
+        echo '<style>.', $nome_produto, ' { display: flex; }</style>';
     }
-    catch(PDOException $erro)
-    {
-        echo("ERRO NA CONEXÃO: <br>".$erro->getMessage());
-    }
+}
+
 ?>
