7ce1c4f5a5b85f38f7b4a84bab68b74ebcd4caa9
HelesT <nyltondudu29@gmail.com>
2023-06-27 09:16:27 -0300

    a

--- confirmação_endereço.php
+++ confirmação_endereço.php
@@ -90,13 +90,6 @@ $totalSoma = $soma + $somaTamanho + $somaAcompanhamento;
         </div>
         <div id="resposta" class="resposta"></div>
     </div>
-    <div class="fale_conoscodiv" id="fale_conoscodiv" style="top: -100%;">
-        <div class="row100">
-            <input type="text" id="mensagem" class="caixadetexto">
-            <button onclick="Enviar();" class="enviartexto">Enviar</button>
-        </div>
-        <div id="resposta" class="resposta"></div>
-    </div>
     <div class="cabecalho">
         <div class="cabecalho1">
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -146,6 +139,42 @@ $totalSoma = $soma + $somaTamanho + $somaAcompanhamento;
                 <img src="carrinho.png" width="40px" >
             </a>
         </div>
+    </div>
+    <div class="caixausu" >
+    <div class="pequenininha2">
+            <?php
+                            include("salvar_imagem.php");
+                            if ($resultado) {
+                                $linha = $comando->fetch(PDO::FETCH_ASSOC);
+                                if ($linha) {
+                                    $dados_imagem = $linha["foto"];
+                                    $i = base64_encode($dados_imagem);        
+                                    // Exibir input de seleção de arquivo como a própria imagem
+                                    echo "<img src='data:image/jpeg;base64,$i' onclick='animar1()' style='border-radius: 50%; object-fit: cover; width: 100%; height: 100%;cursor: pointer;'>";                                    
+                                } else {
+                                    echo "Nenhum arquivo de imagem foi enviado.";
+                                }
+                            } else {
+                                echo "Erro ao recuperar a imagem do banco de dados: " . $pdo->errorInfo()[2];
+                            }
+                        ?>
+            </div>
+            <div style="color:white;margin-top:0px"><?php 
+        include("conecta.php");
+        $query = "SELECT nome_usuario
+          FROM usuario_atual
+          WHERE usuario_atual.acesso = 's'";
+          $resultado = mysqli_query($conexao, $query);
+          if ($resultado && mysqli_num_rows($resultado) > 0) {
+            while ($row = mysqli_fetch_assoc($resultado)) {
+                $nomeUsuario = $row['nome_usuario'];
+            }
+        }
+        echo $nomeUsuario
+        ?></div>   
+        <a href="cadastro2.php" style="margin-top:5px"><button class="Sair2" style="width: 80px;height: 15px;border-radius: 10px;font-size: 10px;display: flex;justify-content: center;justify-items: center;align-content: center;align-items: center;cursor:pointer;">Editar Perfil</button></a>
+        <a href="Login.php" style="margin-top:2px"><button class="Sair2" style="width: 80px;height: 15px;border-radius: 10px;font-size: 10px;display: flex;justify-content: center;justify-items: center;align-content: center;align-items: center;cursor:pointer;">Sair</button></a>                 
+    
     </div>
     <div class="caixausu" >
     <div class="pequenininha2">
@@ -325,40 +354,43 @@ $totalSoma = $soma + $somaTamanho + $somaAcompanhamento;
 <script>
 topico = "nyltoneduardoconstancio";  // Variável que ficará no servidor MQTT
   
-  // Conexão:
-  client = new Paho.MQTT.Client("broker.hivemq.com", Number(8000), "");
+topico = "nyltoneduardoconstancio";
 
-  // Funções executadas quando a conexão é perdida e quando uma mensagem chega:
-  client.onConnectionLost = ConexaoPerdida;
-  client.onMessageArrived = MensagemRecebida;
+    // Conexão:
+    client = new Paho.MQTT.Client("broker.hivemq.com", Number(8000), "");
 
-  // Função chamada quando a conexão for realizada:
-  client.connect({onSuccess:Conectar});
+    // Funções executadas quando a conexão é perdida e quando uma mensagem chega:
+    client.onConnectionLost = ConexaoPerdida;
+    client.onMessageArrived = MensagemRecebida;
 
-  // A função Conectar deve criar a variável (tópico) no computador remoto:
-  function Conectar() {
-      client.subscribe(topico);  // Tópico (variável) criado no servidor MQTT
-  }
+    // Função chamada quando a conexão for realizada:
+    client.connect({onSuccess:Conectar});
 
-  function ConexaoPerdida(responseObject) {
-      if (responseObject.errorCode !== 0) {
-          resposta.innerHTML += "Desconectado";
-      }
-  }
+    // A função Conectar deve criar a variável (tópico) no computador remoto:
+    function Conectar() {
+        client.subscribe(topico);  // Tópico (variável) criado no servidor MQTT
+    }
 
-  // Função executada quando a variável (tópico) no servidor receber uma mensagem:
-  function MensagemRecebida(message) {
-      resposta.innerHTML += "<br><br>" + message.payloadString; 
-      resposta.scrollTo(0, document.body.scrollHeight);
-  }
+    function ConexaoPerdida(responseObject) {
+        if (responseObject.errorCode !== 0) {
+            resposta.innerHTML += "Desconectado";
+        }
+    }
 
-  function Enviar() {
-      texto = mensagem.value;
+    // Função executada quando a variável (tópico) no servidor receber uma mensagem:
+    function MensagemRecebida(message) {
+        resposta.innerHTML += "<br><br>" + message.payloadString; 
+        resposta.scrollTo(0, document.body.scrollHeight);
+    }
 
-      message = new Paho.MQTT.Message(texto);
-      message.destinationName = topico;
-      client.send(message);
-  }
+    function Enviar() {
+        texto = mensagem.value;
+
+        message = new Paho.MQTT.Message(texto);
+        message.destinationName = topico;
+        client.send(message);
+    }
+
 
   var clique = 0;
 
